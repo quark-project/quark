13d35c79612bd82decd6ec662b318c0256c3bd95
MaxGuevara <MaxGuevara>
2013-08-15 23:21:38 +0200

    Fix init

--- src/hashblock.h
+++ src/hashblock.h
@@ -64,77 +64,77 @@ inline uint256 Hash9(const T1 pbegin, const T1 pend)
     
     uint512 hash[9];
 
-    /*sph_blake512_init(&ctx_blake);*/
-    ZBLAKE;
+    sph_blake512_init(&ctx_blake);
+    // ZBLAKE;
     sph_blake512 (&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
     sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
     
-    /*sph_bmw512_init(&ctx_bmw);*/
-    ZBMW;
+    sph_bmw512_init(&ctx_bmw);
+    // ZBMW;
     sph_bmw512 (&ctx_bmw, static_cast<const void*>(&hash[0]), 64);
     sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[1]));
 
     if ((hash[1] & mask) != zero)
     {
-        /*sph_groestl512_init(&ctx_groestl);*/
-        ZGROESTL;
+        sph_groestl512_init(&ctx_groestl);
+        // ZGROESTL;
         sph_groestl512 (&ctx_groestl, static_cast<const void*>(&hash[1]), 64);
         sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[2]));
     }
     else
     {
-        /*sph_skein512_init(&ctx_skein);*/
-        ZSKEIN;
+        sph_skein512_init(&ctx_skein);
+        // ZSKEIN;
         sph_skein512 (&ctx_skein, static_cast<const void*>(&hash[1]), 64);
         sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[2]));
     }
     
-    /*sph_groestl512_init(&ctx_groestl);*/
-    ZGROESTL;
+    sph_groestl512_init(&ctx_groestl);
+    // ZGROESTL;
     sph_groestl512 (&ctx_groestl, static_cast<const void*>(&hash[2]), 64);
     sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[3]));
 
-    /*sph_jh512_init(&ctx_jh);*/
-    ZJH;
+    sph_jh512_init(&ctx_jh);
+    // ZJH;
     sph_jh512 (&ctx_jh, static_cast<const void*>(&hash[3]), 64);
     sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[4]));
 
     if ((hash[4] & mask) != zero)
     {
-        /*sph_blake512_init(&ctx_blake);*/
-        ZBLAKE;
+        sph_blake512_init(&ctx_blake);
+        // ZBLAKE;
         sph_blake512 (&ctx_blake, static_cast<const void*>(&hash[4]), 64);
         sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[5]));
     }
     else
     {
-        /*sph_bmw512_init(&ctx_bmw);*/
-        ZBMW;
+        sph_bmw512_init(&ctx_bmw);
+        // ZBMW;
         sph_bmw512 (&ctx_bmw, static_cast<const void*>(&hash[4]), 64);
         sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[5]));
     }
     
-    /*sph_keccak512_init(&ctx_keccak);*/
-    ZKECCAK;
+    sph_keccak512_init(&ctx_keccak);
+    // ZKECCAK;
     sph_keccak512 (&ctx_keccak, static_cast<const void*>(&hash[5]), 64);
     sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[6]));
 
-    /*sph_skein512_init(&ctx_skein);*/
-    ZSKEIN;
+    sph_skein512_init(&ctx_skein);
+    // SKEIN;
     sph_skein512 (&ctx_skein, static_cast<const void*>(&hash[6]), 64);
     sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[7]));
 
     if ((hash[7] & mask) != zero)
     {
-        /*sph_keccak512_init(&ctx_keccak);*/
-        ZKECCAK;
+        sph_keccak512_init(&ctx_keccak);
+        // ZKECCAK;
         sph_keccak512 (&ctx_keccak, static_cast<const void*>(&hash[7]), 64);
         sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[8]));
     }
     else
     {
-        /*sph_jh512_init(&ctx_jh);*/
-        ZJH;
+        sph_jh512_init(&ctx_jh);
+        // ZJH;
         sph_jh512 (&ctx_jh, static_cast<const void*>(&hash[7]), 64);
         sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[8]));
     }
